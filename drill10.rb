4a7126ec87ebe96378f73fb029e2854452718806
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-01 05:47:53 +0900

    drill10

--- drill10.rb
+++ drill10.rb
@@ -1,4 +1,7 @@
 def search(target_num, input)
+  input.each_with_index do |target_num, input|
+    puts "#{input}番目にあります"
+  end
 end
 
 input = [3, 5, 9, 12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]

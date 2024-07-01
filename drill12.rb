2636b12a9a82bf87e52c268177a9f9e57ad640ae
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-02 06:57:38 +0900

    修正

--- drill12.rb
+++ drill12.rb
@@ -1,13 +1,13 @@
-def judging_the_number(input)
+def judge_number(input)
   if input <= 0
-    puts "0以下の数字です"
+    "0以下の数字です"
   elsif input > 10
-    puts "10より大きい数字です"
+    "10より大きい数字です"
   else
-    puts "10以下の数字です"
+    "10以下の数字です"
   end
 end
 
 puts "数字を入力してください"
 n = gets.to_i
-judging_the_number(n)
+puts judge_number(n)

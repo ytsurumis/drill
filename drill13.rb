255154fd7eb0b039050ff9f832e9bb133910433a
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-02 23:12:05 +0900

    drill13

---
+++ drill13.rb
@@ -0,0 +1,24 @@
+fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
+
+fruits_price.each do |fruit|
+  name = fruit[0]
+  puts "#{name}の値段"
+  prices = fruit[1]
+  prices.each do |price|
+    puts "#{price}円"
+  end
+end
+
+# drill13の答え
+
+fruits_price = [["apple", [200, 250, 220]], ["orange", [100, 120, 80]], ["melon", [1200, 1500]]]
+
+fruits_price.each do |fruit|
+  name = fruit[0]
+  puts "#{name}の値段"
+  prices = fruit[1]
+  prices.each do |price|
+    total_price = 
+    puts "#{price}円"
+  end
+end

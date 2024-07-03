47fa4868f3e59c33b2af7cdfcce6b41ee26b71af
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-04 06:45:25 +0900

    修正

--- drill14.rb
+++ drill14.rb
@@ -1,16 +1,16 @@
 def in1to10(num, outside_mode)
-  if outside_mode == "True"
+  if outside_mode
     puts "True"
-  elsif num => 1 && =< 10
+  elsif num >= 1 && num <= 10
     puts "True"
   else
     puts "False"
   end
 end
 
-num = gets.to_i
 puts "数字を入力してください"
-outside_mode = gets
+num = gets.to_i
 puts "trueかfalseを入力してください"
+outside_mode = gets
 
 in1to10(num, outside_mode)

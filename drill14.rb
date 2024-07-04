2d6cde27c6c06901af7d162f0d6c8fcaaf2e8b1a
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-04 20:05:39 +0900

    模範解答

--- drill14.rb
+++ drill14.rb
@@ -15,3 +15,21 @@ outside_mode_input = gets.chomp.downcase
 outside_mode = outside_mode_input == "true"
 
 in1to10(num, outside_mode)
+
+# 模範解答
+
+# def in1to10(num, outside_mode)
+#   if(num >=1 && num <= 10) || outside_mode
+#     puts "True"
+#   else
+#     puts "False"
+#   end
+# end
+
+# puts "数字を入力してください"
+# num = gets.to_i
+# puts "trueかfalseを入力してください"
+# outside_mode_input = gets.chomp.downcase
+# outside_mode = outside_mode_input == "true"
+
+# in1to10(num, outside_mode)

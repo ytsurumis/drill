e0d69c8ccafb267bc84ae350c81376fbc78f1660
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-09 22:13:10 +0900

    別の記述

--- drill18.rb
+++ drill18.rb
@@ -1,16 +1,33 @@
-def police_trouble(a, b)
-  a = a == 'true'
-  b = b == 'true'
+# def police_trouble(a, b)
+#   a = a == 'true'
+#   b = b == 'true'
 
-  if ( a && b ) || ( !a && !b )
+#   if ( a && b ) || ( !a && !b )
+#     puts true
+#   else
+#     puts false
+#   end
+# end
+
+# puts "証言a(trueまたはfalseを入力してください)"
+# a = gets.chomp
+# puts "証言b(trueまたはfalseを入力してください)"
+# b = gets.chomp
+# police_trouble(a, b)
+
+# 同じことを言っていたらtrueを返す、違うことを言っていたらfalseを返すというコード
+
+def police_trouble(a, b)
+  if a == b
     puts true
   else
     puts false
   end
 end
-
-puts "証言a(trueまたはfalseを入力してください)"
+  
+puts '証言を入力してください'
 a = gets.chomp
-puts "証言b(trueまたはfalseを入力してください)"
+puts '別の人の証言を入力してください'
 b = gets.chomp
+
 police_trouble(a, b)

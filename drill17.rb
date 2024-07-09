9d8905f2cd3ce9d486bf61a7d00280b20b008e5a
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-09 20:29:16 +0900

    drill17 修正③

--- drill17.rb
+++ drill17.rb
@@ -1,6 +1,11 @@
 def missing_char(str, n)
-  str.slice!(n) # strのn番目の文字を削除する
+  str.slice!(n-1) # strのn番目の文字を削除する
   puts str      # 削除後のstrを出力する
 end
 
-missing_char('kitten', 1)
+puts "文字列を入力してください"
+str = gets.chomp
+puts "数字を入力してください"
+n = gets.to_i
+
+missing_char('kitten', n)

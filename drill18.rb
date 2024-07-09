030dbcc90b08349684635c9c0a3c0d36370c233a
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-09 22:03:40 +0900

    drill18 修正

--- drill18.rb
+++ drill18.rb
@@ -1,13 +1,16 @@
 def police_trouble(a, b)
+  a = a == 'true'
+  b = b == 'true'
+
   if ( a && b ) || ( !a && !b )
-    puts "True"
+    puts true
   else
-    puts "False"
+    puts false
   end
 end
 
-puts "証言a"
+puts "証言a(trueまたはfalseを入力してください)"
 a = gets.chomp
-puts "証言b"
+puts "証言b(trueまたはfalseを入力してください)"
 b = gets.chomp
 police_trouble(a, b)

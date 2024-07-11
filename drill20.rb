83fd8b2ac780f7f6f885eb41eb205396a586f597
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-12 06:26:13 +0900

    drill20 修正 2

--- drill20.rb
+++ drill20.rb
@@ -1,7 +1,12 @@
 def find_code(str)
-  str.index('code')
+  index = str.index('code')
+  if index.nil?
+    puts "文字列に'code`は含まれていません"
+  else
+    puts "'code'は文字列の#{index + 1}文字目にあります。"
+  end
 end
 
 puts "文字列を入力してください"
 input_str = gets.chomp
-puts find_code(input_str) + 1
+find_code(input_str)

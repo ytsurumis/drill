3db7caeee17aa9df0f7323cb5d65d73bf5bd61b5
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-14 06:40:11 +0900

    drill21

--- drill21.rb
+++ drill21.rb
@@ -1,2 +1,12 @@
+def parrot_trouble(talking, hour)
+  if talking == false
+    puts "OK"
+  elsif hour => 7 && hour =< 20
+    puts "OK"
+  elsif talking == true && (hour < 7 || hour > 20)
+    puts "NG"
+  end  
+end
 
+parrot_trouble(true, 6)
  

61a7818ee899b381e922ccfb8ad854b584004e7d
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-18 06:48:09 +0900

    簡潔なコード

--- drill23.rb
+++ drill23.rb
@@ -1,11 +1,17 @@
+# def array123(nums)
+#   if nums.include?(1) && nums.include?(2) && nums.include?(3)
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+# 条件式の結果を直接返すことで、コードを簡潔にすることができます。
 def array123(nums)
-  if nums.include?(1) && nums.include?(2) && nums.include?(3)
-    return true
-  else
-    return false
-  end
+  nums.include?(1) && nums.include?(2) && nums.include?(3)
 end
 
 puts array123([1, 1, 2, 3, 1])
 puts array123([1, 2, 4])
 puts array123([1, 1, 2, 1, 4, 3])
+

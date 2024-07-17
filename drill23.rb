6199cd4626b76b4b7920942d2c3a436a3e51f677
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-18 06:33:08 +0900

    drill23

---
+++ drill23.rb
@@ -0,0 +1,8 @@
+def array123(nums)
+  if nums.include?(1) && nums.include?(2) && nums.include?(3)
+  end
+end
+
+puts array123([1, 1, 2, 3, 1])
+puts array123([1, 2, 4])
+puts array123([1, 1, 2, 1, 4, 3])

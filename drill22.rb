78cd758da0fd3fe5cf6402577332b187729fbe27
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-18 06:07:25 +0900

    drill22 修正

--- drill22.rb
+++ drill22.rb
@@ -1,3 +1,13 @@
 def count_evens(array)
+  count = 0
   array.each do |num|
-    num.even?
+    if num.even?
+      count += 1
+    end
+  end
+  count
+end
+
+puts count_evens([2, 1, 2, 3, 4])
+puts count_evens([2, 2, 0])
+puts count_evens([1, 3, 5])

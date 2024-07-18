f3aa8fa8997b75feebad91365959ba14f3faedf8
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-19 06:40:26 +0900

    修正drill24

--- drill24.rb
+++ drill24.rb
@@ -1,9 +1,12 @@
 def sleep_in(is_weekday, is_vacation)
   if !is_weekday || is_vacation
     return true
+  else
+    return false
+  end
 end
 
-puts sleep_in (false, false) # => true
+puts sleep_in(false, false) # => true
 puts sleep_in(true, false) # => false
 puts sleep_in(false, true) # => true
 puts sleep_in(true, true) # => true

c1d981fa31527b8f6f564f07329114c6eb2ea4bb
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-20 06:49:10 +0900

    修正drill25

--- drill25.rb
+++ drill25.rb
@@ -1,29 +1,28 @@
 def addition(a, b)
-  # 10の位と1の位の数字に対して、足し算を行う
   a + b
 end
 
 def multiplication(a, b)
-  # 10の位と1の位の数字に対して、掛け算を行う
   a * b
 end
 
 def slice_num(num)
-  #10の位の計算
   x = num / 10
-  #1の位の計算
   y = num % 10
+  return x, y # 10の位と1の位の計算を行い、その結果を返す
 end
 
 puts "二桁の整数を入力してください"
 input = gets.to_i
 
-x, y = slice_num(input)
+if input < 10
+  puts "二桁以上の整数を入力してください"
+else
+  x, y = slice_num(input)
 
-# additionメソッドにx, yを引数としてわたし、処理結果を変数add_resultに代入する
-add_result = addition(x, y)
+  add_result = addition(x, y)
 
-# multiplicationメソッドにx, yを引数としてわたし、処理結果を変数multiple_resultに代入する
-multiple_result = multiplication(x, y)
+  multiple_result = multiplication(x, y)
 
-puts "足し算結果と掛け算結果の合計値は#{add_result + mutiple_resunt}です"
+  puts "足し算結果と掛け算結果の合計値は#{add_result + multiple_result}です"
+end

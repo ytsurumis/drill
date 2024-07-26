3280b36e327734f218a2a3268987c2d09c9f3f90
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-07-27 00:00:32 +0900

    drill30.rb

---
+++ drill30.rb
@@ -0,0 +1,22 @@
+Fizz = 3
+Buzz = 5
+FizzBuzz = 15
+
+def fizzbuzz(max_num)
+  max_num.times do |i|
+    number = i + 1
+    if number = FizzBuzz
+      puts 'FizzBuzz'
+    elsif number = Buzz
+      puts 'Buzz'
+    elsif number = Fizz
+      puts 'Fizz'
+    else
+      number
+    end
+  end
+end
+
+puts 'いくつまで数えますか？'
+num = gets.to_i
+fizzbuzz(num)

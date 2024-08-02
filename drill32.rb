2e918d56b3b68ce1aaecf02347533acf14edaaba
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-08-03 02:07:06 +0900

    修正 drill32

--- drill32.rb
+++ drill32.rb
@@ -3,7 +3,7 @@ class Student
     @name = name
   end
 
-  def self.introduce
+  def introduce
     puts "私の名前は#{@name}です。"
   end
 end
@@ -11,3 +11,23 @@ end
 student = Student.new
 student.set_name("つるみそうた")
 student.introduce
+
+# class MyCar
+#   def set_color(color)
+#     @color = color
+#   end
+
+#   def set_weight(weight)
+#     @weight = weight
+#   end
+
+#   def show_details
+#     puts "Color: #{@color}, Weight: #{@weight}"
+#   end
+# end
+
+# my_car = MyCar.new
+# my_car.set_color("red")
+# my_car.set_weight(2000)
+# my_car.show_details
+

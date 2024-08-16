8224d25b1ce3a2d46356da3c19d030b4805b0685
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-08-16 10:32:20 +0900

    修正　drill36

--- drill36.rb
+++ drill36.rb
@@ -1,13 +1,16 @@
 def withdraw(balance, amount)
-  fee = 100
-  if balance < (amount + fee)
+  fee = 110
+  total_withdrawal = amount + fee
+  if balance < total_withdrawal
     puts "残高不足です"
   else
-    puts "#{amount}円引き落としました。残高は#{balance-(amount + fee)}円です"
+    balance -= total_withdrawal
+    puts "#{amount}円引き落としました。残高は#{balance}円です"
   end
+  balance
 end
 
 balance = 10000
 puts "いくら引き落としますか？（手数料110円かかります）"
 money = gets.to_i
-withdraw(balance, money)
+balance = withdraw(balance, money)
